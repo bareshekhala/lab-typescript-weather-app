48fc209181bfbad092d8aba810cc3ed6e6372b7e
mobinaj94 <mobinajafari94@yahoo.com>
2026-09-24 23:29:57 +0200

    1-4 done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
-// src/main.ts
+//
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeatherData,
+} from "./utils";
+
+
+const form = document.querySelector<HTMLFormElement>("#weather-form");
+
+form?.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const locationInput =
+    document.querySelector<HTMLInputElement>("#location");
+
+  if (!locationInput) return;
+
+  const locationName = locationInput.value;
+
+  locationInput.value = "";
+
+  try {
+    const response = await getLocation(locationName);
+
+    if (!response.results || response.results.length === 0) {
+      throw new Error("Location not found");
+    }
+
+    const location = response.results[0];
+
+    displayLocation(location);
+
+    const weatherData = await getCurrentWeather(location);
+
+    displayWeatherData(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,51 @@
 // src/utils.ts
 
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
 
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
     const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
     return axios.get(url).then((response) => response.data);
+}
+//
+export async function getCurrentWeather(
+  locationDetails: Location
+): Promise<WeatherResponse> {
+
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+
+  const response = await axios.get(url);
+    return response.data;
+}
+//
+export function displayLocation(locationDetails: Location): void {
+  const locationNameElm = document.querySelector<HTMLElement>("#location-name");
+
+  const countryElm = document.querySelector<HTMLElement>("#country");
+
+  if (locationNameElm && countryElm) {
+    locationNameElm.innerText = locationDetails.name;
+    countryElm.innerText = `(${locationDetails.country})`;
+  }
+}
+//
+export function displayWeatherData(obj: WeatherResponse): void {
+  const temperatureElm = document.querySelector<HTMLElement>("#temperature");
+  const windspeedElm = document.querySelector<HTMLElement>("#windspeed");
+  const winddirectionElm = document.querySelector<HTMLElement>("#winddirection");
+
+  if (temperatureElm && windspeedElm && winddirectionElm) {
+    temperatureElm.innerText =
+      `Temperature: ${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`;
+
+    windspeedElm.innerText =
+      `Wind Speed: ${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`;
+
+    winddirectionElm.innerText =
+      `Wind Direction: ${obj.current_weather.winddirection} ${obj.current_weather_units.winddirection}`;
+  }
 }
 
 
